925a6e65f41c15e0c9cca1397ae8248d435ead0f
Christine Contreras <christineec23@yahoo.com>
2021-09-14 14:15:37 -0500

    working

--- back-end/db/seeds.rb
+++ back-end/db/seeds.rb
@@ -5,9 +5,25 @@ Task.destroy_all
 puts "Seeding Categories..."
 
 # create categories
+food = Category.create(name: 'Food')
+money = Category.create(name: 'Money')
+code = Category.create(name: 'Code')
+misc = Category.create(name: 'Misc')
+Category.create(name: 'School')
 
 puts "Seeding tasks..."
 
 # create tasks
+# can also build this way and save 
+food.tasks.build(text: "buy blueberries")
+food.save
+
+Task.create(text: 'Buy rice', category_id: food.id)
+Task.create(text: 'Save a tenner', category_id: money.id)
+Task.create(text: 'Build a todo app', category_id: code.id)
+Task.create(text: 'Build todo API', category_id: code.id)
+Task.create(text: 'Get an ISA', category_id: money.id)
+Task.create(text: 'Tidy house', category_id: misc.id)
+
 
 puts "Done!"
